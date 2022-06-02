61b0d98673c4a63b2788feeadf9344331db5f327
Rodrigo Augusto Reis <rodrigoreis@rodrigos-mbp.lan>
2022-06-02 19:03:52 +1200

    add server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const fs = require('fs')
+const server = express()
+
+// Server configuration
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+
+// Your routes/router(s) should go here
+server.use('/puppies', routers)
+
+server.get('/', (req, res) => {
+  fs.readFile('./data.json', 'utf-8', (err, data) => {
+   res.render('home', JSON.parse(data))
+  })
+})
+
+
+//ViewData
+server.get('/', (req, res) => {
+  const viewData = {
+    movies: movies,
+  }
+
+  const template = 'home'
+  res.render(template, viewData)
+})
+
+
+
+
+
+module.exports = server
