209a0bee203082058311df595da5305007d5f260
Rodrigo Augusto Reis <rodrigoreis@rodrigos-mbp.lan>
2022-06-03 10:49:15 +1200

    Working on handlebars

--- server.js
+++ server.js
@@ -4,15 +4,48 @@ const fs = require('fs')
 const server = express()
 const movies = require('./movies.json')
 const path = require('path')
+const movieRoute = require('./routes')
 
 // Server configuration
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
+server.use('/moviedetails', movieRoute)
 
 // Handlebars configuration
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
+//const movieRoute = require('./routes')
+
+// Render movies on
+server.get('/', (req, res) => {
+  let filePath = path.resolve('./movies.json')
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.err(err.message)
+      return
+    }
+    const movies = JSON.parse(data)
+    res.render('home', movies)
+    // console.log(movies)
+  })
+})
+
+// server.get('/', (req, res) => {
+//   res.redirect('/moviesdetails')
+// })
+
+//ViewData
+// server.get('/', (req, res) => {
+//   const viewData = {
+//     movies: movies,
+//   }
+
+//   const template = 'home'
+//   res.render(template, viewData)
+// })
+
+module.exports = server
 // Your routes/router(s) should go here
 // server.use('/puppies', routers)
 
@@ -25,35 +58,3 @@ server.set('view engine', 'hbs')
 //    res.render('home', JSON.parse(data))
 //   })
 // })
-
-// Render puppies on 
-server.get('/', (req, res) => {
-  let filePath = path.resolve('./movies.json')
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.err(err.message)
-      return
-    }
-    const movies = JSON.parse(data)
-    res.render('home', movies)
-    // console.log(movies)
-    //res.send(puppies)
-  })
-})
-
-
-//ViewData
-// server.get('/', (req, res) => {
-//   const viewData = {
-//     movies: movies,
-//   }
-
-//   const template = 'home'
-//   res.render(template, viewData)
-// })
-
-
-
-
-
-module.exports = server
