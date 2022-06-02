6c8de097cd739813b5b72c2ce39431d59309f0db
Rodrigo Augusto Reis <rodrigoreis@rodrigos-mbp.lan>
2022-06-02 19:33:18 +1200

    add express, server working

--- server.js
+++ server.js
@@ -12,24 +12,28 @@ server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-server.use('/puppies', routers)
+// server.use('/puppies', routers)
 
 server.get('/', (req, res) => {
-  fs.readFile('./data.json', 'utf-8', (err, data) => {
-   res.render('home', JSON.parse(data))
-  })
+  res.send('Hello world')
 })
 
+// server.get('/', (req, res) => {
+//   fs.readFile('./movies.json', 'utf-8', (err, data) => {
+//    res.render('home', JSON.parse(data))
+//   })
+// })
 
-//ViewData
-server.get('/', (req, res) => {
-  const viewData = {
-    movies: movies,
-  }
 
-  const template = 'home'
-  res.render(template, viewData)
-})
+// //ViewData
+// server.get('/', (req, res) => {
+//   const viewData = {
+//     movies: movies,
+//   }
+
+//   const template = 'home'
+//   res.render(template, viewData)
+// })
 
 
 
