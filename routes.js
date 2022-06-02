5091bfaee25199e12561ad1d817875fc2d1909f7
kate-stretton <81283807+kate-stretton@users.noreply.github.com>
2022-06-03 10:27:31 +1200

    Complete moviedetail route

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const res = require('express/lib/response')
+// const res = require('express/lib/response')
 const router = express.Router()
 const fs = require('fs')
 const path = require('path')
@@ -8,10 +8,13 @@ const path = require('path')
 
 const movieData = path.resolve('./data.json')
 
-router.get('/moviedetails/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const id = req.params.id
   showOneMovie(id, (movies) => {
+    console.log('movies', movies)
+    // res.render('home', movies)
     res.render('moviedetails', movies)
+    console.log('here')
   })
 })
 
